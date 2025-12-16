b2946e3f5f2fd456db1bf29b8e7f456c6961e5d1
Allison Borba Cabral <allisonborba@hotmail.com>
2025-12-16 01:08:42 -0300

    Refatoração Payment Method

--- masterdata/urls/paymentmethod.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from masterdata.views.paymentmethod import index
-
-urlpatterns = [
-    path('', index, name='index'),
-]
--- masterdata/views/paymentmethod.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from masterdata.models.paymentmethod import PaymentMethod
-
-# Create your views here.
-def index(request):
-    pass
